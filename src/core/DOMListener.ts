48174437451fbbef73204b0ae200f6c52411b660
97Dmitry <dart_veyder_1997@mail.ru>
2021-06-04 16:57:42 +0600

    Added some functional for core Classes

--- src/core/DOMListener.ts
+++ src/core/DOMListener.ts
@@ -1,10 +1,38 @@
-export class DOMListener {
-  private $source: Element;
+import { DOM } from "./DOM";
+import { capitalize } from "./utils";
 
-  constructor($source: Element) {
+export class DOMListener {
+  protected $source: DOM;
+  private listeners: Array<string>;
+  protected componentName: string;
+
+  constructor($source: DOM, listeners:Array<string> = []) {
     if (!$source) {
       throw new Error("No $source provided for DOMListener");
     }
     this.$source = $source;
+    this.listeners = listeners;
   }
+
+  initialDOMListeners(): void {
+    this.listeners.forEach((listener) => {
+      const method: string = getMethodName(listener);
+      if (!(this as any)[method]) {
+        throw new Error(`Method ${method} does not exist on component ${this.componentName}`);
+      }
+      (this as any)[method] = (this as any)[method].bind(this);
+      this.$source.ael(listener, (this as any)[method]);
+    });
+  }
+
+  removeDOMListeners(): void {
+    this.listeners.forEach((listener) => {
+      const method: string = getMethodName(listener);
+      this.$source.rel(listener, (this as any)[method]);
+    });
+  }
+}
+
+function getMethodName(eventMethod: string): string {
+  return ("on" + capitalize(eventMethod));
 }
